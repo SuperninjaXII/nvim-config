c2fdff1d6dfceebedde40dcdacb5b25b89597c05
snx2 <lunguj665@gmail.com>
2026-02-12 05:51:43 +0200

    added colorizer

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -17,5 +17,11 @@ return{
         lazy = false, -- Almost component is lazy load by default. So you can set lazy to false
         opts = {},
     },
-
+{
+    "catgoose/nvim-colorizer.lua",
+    event = "BufReadPre",
+    opts = { -- set to setup table
+    },
+},
+{ 'nvim-mini/mini.nvim', version = '*' },
 }
